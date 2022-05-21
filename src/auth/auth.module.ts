7d8fd2fd941070a0f4f7f5187a181f9e8e04f476
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2022-05-21 11:02:59 -0300

    feat: connected to db one time

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [AuthController],
-  providers: [AuthService, PrismaService],
+  providers: [AuthService],
 })
 export class AuthModule {}
--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -4,11 +4,10 @@ import { ProjectsController } from './projects.controller';
 import { AwsS3Module } from 'src/aws-s3/aws-s3.module';
 import { AwsS3Service } from 'src/aws-s3/aws-s3.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  controllers: [ProjectsController],
-  providers: [ProjectsService, AwsS3Service, PrismaService],
   imports: [AwsS3Module, PrismaModule],
+  controllers: [ProjectsController],
+  providers: [ProjectsService, AwsS3Service],
 })
 export class ProjectsModule {}
